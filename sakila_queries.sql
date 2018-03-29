7a4526a2a11bc54ad825ca0a84bcf0d632446da9
Jayleh <jaylehyun@gmail.com>
2018-03-29 15:39:34 -0700

    completed queries

--- sakila_queries.sql
+++ sakila_queries.sql
@@ -1,10 +1,11 @@
+-- Use sakila database
 USE sakila;
 
 -- 1a. Display first and last names of all actors
 SELECT first_name, last_name FROM actor;
 
 -- 1b. Display first and last name in a single column
-SELECT CONCAT(first_name, ' ', last_name) AS 'Actor Name' FROM actor ;
+SELECT CONCAT(first_name, ' ', last_name) AS 'Actor Name' FROM actor;
 
 -- 2a. Find the ID number, first name, and last name of an actor, of whom you know only the first name, 'Joe'
 SELECT actor_id, first_name, last_name FROM actor
@@ -40,7 +41,7 @@ DROP COLUMN middle_name;
 SELECT last_name, COUNT(*) AS count FROM actor
 GROUP BY last_name;
 
--- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+-- 4b. List last names of actors and the number of actors who have that last name, but only those by at least two actors
 SELECT last_name, COUNT(*) AS count FROM actor
 GROUP BY last_name
 HAVING count > 1;
@@ -64,12 +65,118 @@ SHOW CREATE TABLE sakila.address;
 -- 6a. Display staff members' addresses
 SELECT s.first_name, s.last_name, a.address
 FROM staff s
-JOIN address a
-USING (address_id);
+	JOIN address a USING (address_id);
 
--- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment
+-- 6b. Display the total amount rung up by each staff member in August of 2005
+SELECT s.first_name, s.last_name, SUM(p.amount) AS 'total_amount'
+FROM staff s
+	JOIN payment p USING (staff_id)
+WHERE p.payment_date LIKE '2005-08%'
+GROUP BY staff_id;
 
+-- 6c. List each film and the number of actors who are listed for that film
+SELECT f.title, COUNT(fa.film_id) AS 'Number of actors'
+FROM film f
+	JOIN film_actor fa USING (film_id)
+GROUP BY f.title;
 
+-- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
+SELECT COUNT(*) FROM inventory
+WHERE film_id IN (
+	SELECT film_id FROM film
+	WHERE title = 'HUNCHBACK IMPOSSIBLE'
+);
 
+-- 6e. List the total paid by each customer. List the customers alphabetically by last name
+SELECT c.first_name, c.last_name, SUM(p.amount) AS 'Total Amount Paid'
+FROM customer c
+	JOIN payment p USING (customer_id)
+GROUP BY customer_id
+ORDER BY last_name;
 
+-- 7a. Display the titles of movies starting with the letters K and Q whose language is English
+SELECT title FROM film
+WHERE title LIKE 'K%' OR title LIKE 'Q%' AND language_id IN (
+	SELECT language_id FROM language
+    WHERE name = 'English'
+);
 
+-- 7b. Display all actors who appear in the film Alone Trip
+SELECT first_name, last_name FROM actor
+WHERE actor_id IN (
+	SELECT actor_id FROM film_actor
+    WHERE film_id IN (
+		SELECT film_id FROM film
+        WHERE title = 'Alone Trip'
+    )
+);
+
+-- 7c. Display the names and email addresses of all Canadian customers
+SELECT cust.first_name, cust.last_name, cust.email
+FROM customer cust
+	JOIN address USING (address_id)
+    JOIN city USING (city_id)
+    JOIN country USING (country_id)
+WHERE country = 'Canada';
+
+-- 7d. Identify all movies categorized as family films
+SELECT title FROM film
+WHERE film_id IN (
+	SELECT film_id FROM film_category
+    WHERE category_id IN (
+		SELECT category_id FROM category
+        WHERE name = 'Family'
+	)
+);
+
+-- 7e. Display the most frequently rented movies in descending order
+SELECT f.title, COUNT(p.rental_id) AS 'Number of Times Rented'
+FROM film f
+	JOIN inventory USING (film_id)
+    JOIN rental USING (inventory_id)
+    JOIN payment p USING (rental_id)
+GROUP BY f.title
+ORDER BY `Number of Times Rented` DESC;
+
+-- 7f. Display how much business, in dollars, each store brought in
+SELECT staff.store_id, SUM(payment.amount) AS 'Total Revenue'
+FROM staff
+	JOIN rental USING (staff_id)
+    JOIN payment USING (rental_id)
+GROUP BY staff.store_id;
+
+-- 7g. Display for each store its store ID, city, and country
+SELECT s.store_id, city.city, country.country
+FROM store s
+	JOIN address USING (address_id)
+    JOIN city USING (city_id)
+    JOIN country USING (country_id);
+
+-- 7h. List the top five genres in gross revenue in descending order
+SELECT cat.name, SUM(p.amount) AS 'Gross Revenue'
+FROM category cat
+	JOIN film_category USING (category_id)
+    JOIN inventory USING (film_id)
+    JOIN rental USING (inventory_id)
+    JOIN payment p USING (rental_id)
+GROUP BY cat.name
+ORDER BY `Gross Revenue` DESC
+LIMIT 5;
+
+-- 8a. Create a view of the top five genres by gross revenue
+CREATE VIEW top_five_genres AS
+SELECT cat.name, SUM(p.amount) AS 'Gross Revenue'
+FROM category cat
+	JOIN film_category USING (category_id)
+    JOIN inventory USING (film_id)
+    JOIN rental USING (inventory_id)
+    JOIN payment p USING (rental_id)
+GROUP BY cat.name
+ORDER BY `Gross Revenue` DESC
+LIMIT 5;
+
+-- 8b. Display top_five_genres view
+SELECT * FROM top_five_genres;
+
+-- 8c. Delete top_five_genres view
+DROP VIEW IF EXISTS top_five_genres;
